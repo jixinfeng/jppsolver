6f210930e283a98f138fc3cc8f59ae8925110c98
Jixin Feng <ufjfeng@users.noreply.github.com>
2017-07-13 23:36:34 +0800

    Create base class for future implementations

---
+++ JppSolver.py
@@ -0,0 +1,86 @@
+import networkx as nx
+import numpy as np
+
+
+class JppSolver(object):
+    DEFAULT_GRAPH_ORDER = 100
+    DEFAULT_COMM_RADIUS = 0.15
+
+    def __init__(self):
+        self.graph = nx.Graph()
+        self.order = 0
+        self.pos = {}
+        self.comm_radius = 0
+        self.jam_radius = 0
+        self.solns = {}
+
+    def set_graph(self,
+                  graph,
+                  comm_radius,
+                  jam_radius=None):
+        if not nx.is_connected(graph):
+            raise AttributeError("Graph not connected!")
+        self.reset()
+        self.graph = graph
+        self.order = graph.order()
+        self.pos = nx.get_node_attributes(graph, 'pos')
+        if graph.order() != len(self.pos):
+            raise AttributeError("Node position does not match!")
+        self.comm_radius = comm_radius
+        if jam_radius is None:
+            self.jam_radius = comm_radius
+        else:
+            self.jam_radius = jam_radius
+
+    def set_graph_via_pos(self,
+                          pos,
+                          comm_radius,
+                          jam_radius=None):
+        if jam_radius is None:
+            jam_radius = comm_radius
+        graph = nx.random_geometric_graph(n=len(pos),
+                                          radius=comm_radius,
+                                          pos=pos)
+        self.set_graph(graph, comm_radius, jam_radius)
+
+    def set_random_graph(self,
+                         order=100,
+                         comm_radius=0.15,
+                         jam_radius=None):
+        self.reset()
+        self.graph = self._generate_connected_graph(order, comm_radius)
+        self.order = order
+        self.pos = nx.get_node_attributes(self.graph, 'pos')
+        self.comm_radius = comm_radius
+        if jam_radius is None:
+            self.jam_radius = comm_radius
+        else:
+            self.jam_radius = jam_radius
+
+    def solve(self):
+        return self.solns
+
+    def reset(self):
+        self.graph = nx.Graph()
+        self.order = 0
+        self.pos = {}
+        self.comm_radius = 0
+        self.jam_radius = 0
+        self.solns = {}
+
+    def _generate_connected_graph(self, order, comm_radius):
+        mag_ratio = np.sqrt(order / self.DEFAULT_GRAPH_ORDER)
+        if comm_radius < self.DEFAULT_COMM_RADIUS:
+            print("WARNING: this order, comm_radius combination may cause unpredictable long running time")
+
+        graph = nx.random_geometric_graph(order, comm_radius / mag_ratio)
+        while not nx.is_connected(graph):
+            graph = nx.random_geometric_graph(order, comm_radius / mag_ratio)
+
+        pos = nx.get_node_attributes(graph, 'pos')
+        for i in range(order):
+            pos[i] = tuple(j * mag_ratio for j in pos[i])
+
+        return nx.random_geometric_graph(n=order,
+                                         radius=comm_radius,
+                                         pos=pos)
--- JppSolverIlp.py
+++ JppSolverIlp.py
@@ -1,63 +1,17 @@
+import cvxopt
 import networkx as nx
 import numpy as np
-import cvxopt
 from cvxopt.glpk import ilp
 from scipy.sparse import coo_matrix
 
+from JppSolver import JppSolver
 
-class JppSolverIlp(object):
+
+class JppSolverIlp(JppSolver):
     def __init__(self, verbose=False):
-        self.graph = nx.Graph()
-        self.order = 0
-        self.pos = {}
-        self.comm_radius = 0
-        self.jam_radius = 0
-        self.solns = {}
+        JppSolver.__init__(self)
         if not verbose:
             cvxopt.glpk.options['msg_lev'] = 'GLP_MSG_OFF'
-
-    def set_graph(self,
-                  graph,
-                  comm_radius,
-                  jam_radius=None):
-        if not nx.is_connected(graph):
-            raise AttributeError("Graph not connected!")
-        self.reset()
-        self.graph = graph
-        self.order = graph.order()
-        self.pos = nx.get_node_attributes(graph, 'pos')
-        if graph.order() != len(self.pos):
-            raise AttributeError("Node position does not match!")
-        self.comm_radius = comm_radius
-        if jam_radius is None:
-            self.jam_radius = comm_radius
-        else:
-            self.jam_radius = jam_radius
-
-    def set_graph_via_pos(self,
-                          pos,
-                          comm_radius,
-                          jam_radius=None):
-        if jam_radius is None:
-            jam_radius = comm_radius
-        graph = nx.random_geometric_graph(n=len(pos),
-                                          radius=comm_radius,
-                                          pos=pos)
-        self.set_graph(graph, comm_radius, jam_radius)
-
-    def set_random_graph(self,
-                         order=100,
-                         comm_radius=0.15,
-                         jam_radius=None):
-        self.reset()
-        self.graph = self._genConnectedGraph(order, comm_radius)
-        self.order = order
-        self.pos = nx.get_node_attributes(self.graph, 'pos')
-        self.comm_radius = comm_radius
-        if jam_radius is None:
-            self.jam_radius = comm_radius
-        else:
-            self.jam_radius = jam_radius
 
     def solve(self, k):
         if k in self.solns:
@@ -118,14 +72,6 @@ class JppSolverIlp(object):
         return {'jammer': jammer,
                 'jammed': jammed,
                 'clusters': clusters}
-
-    def reset(self):
-        self.graph = None
-        self.order = None
-        self.pos = None
-        self.comm_radius = None
-        self.jam_radius = None
-        self.solns = {}
 
     def _parse(self, isol):
         soln = []
